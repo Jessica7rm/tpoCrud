566ef02ff0f3017c8d7be16d5a037878259bf95d
Jessica <jessica7rm@gmail.com>
2023-07-06 01:12:07 +0200

    commit_footer

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ navbar" style="background-color: #e3f2fd;">
 
 </div>
 </nav>`
+
+document.getElementById("footer").innerHTML=`<div class="iconos">
+<a href="https://www.instagram.com/jessica7rm/" target="_blank"><img src="./img/instagram.png" alt="instagram"></a>
+<a href="https://www.linkedin.com/in/jessica7rm/" target="_blank"><img src="./img/linkedin.png" alt="linkedin"></a>
+<a href="mailto:jessica7rm@gmail.com"><img src="./img/mail.png" alt="email"></a>
+</div>
+ <p class="ftext">Jessica Huertas 2023 // iconos flaticon.com</p>`
